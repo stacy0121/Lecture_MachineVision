6c171b4cb7c3e62dce63f50900ca5c43f479510d
stacy0121 <stcie12@gmail.com>
2022-06-16 23:08:52 +0900

    chap07

--- chap07/00.canny1.py
+++ chap07/00.canny1.py
@@ -1,4 +1,4 @@
-import cv2, sys
+import numpy as np, cv2, sys
 import matplotlib.pyplot as plt
 
 src = cv2.imread("chapter7images/campusLibrary2.png", cv2.IMREAD_GRAYSCALE)
@@ -9,7 +9,13 @@ if src is None:
 
 dst1 = cv2.Canny(src, 50, 200)
 dst2 = cv2.Canny(src, 120, 200)
-dst3 = cv2.Canny(src, 180, 200)
+dst3 = cv2.Canny(src, 180, 200)   # 엣지 검출
+dst3 = cv2.threshold(dst3, 128, 255, cv2.THRESH_BINARY)[1]   # 이진화
+data = [[0, 1, 0],
+        [1, 1, 1],
+        [0, 1, 0]]
+mask = np.array(data, np.uint8).reshape(3, 3)
+dst3 = cv2.dilate(dst3, mask)   # 팽창
 titles = ['src', 'dst1', 'dst2', 'dst3']
 images = [src, dst1, dst2, dst3]
 
